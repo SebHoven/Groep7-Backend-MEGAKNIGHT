e4e18241a005496d0c0b287eab51abd523ddfacb
Jero <exte0005@hz.nl>
2025-11-27 11:41:10 +0100

    crud controller

---
+++ groups/code/src/controllers/tasksController.ts
@@ -0,0 +1,101 @@
+import { Request, Response } from 'express';
+import { PrismaClient } from '@prisma/client';
+import { Task } from '../../prisma/types.js';
+const prisma: PrismaClient = new PrismaClient();
+
+/**
+ * Interface for the response object
+ */
+interface TaskResponse {
+    meta: {
+        count: number
+        title: string
+        url: string
+    },
+    data: Task[]
+}
+
+export const getAllTasks = async (req: Request, res: Response) => {
+    try {
+        const tasks = await prisma.task.findMany();
+        const taskResponse: TaskResponse = {
+            meta: {
+                count: tasks.length,
+                title: 'All tasks',
+                url: req.url
+            },
+            data: tasks
+        };
+        res.json(taskResponse);
+    } catch (errors) {
+        res.status(500).json({ error: 'kan geen taken vinden' });
+    }
+};
+
+export const getTaskById = async (req: Request, res: Response) => {
+    try {
+        const id = req.params.id;
+        const tasks = await prisma.task.findUnique({
+            where: { id: Number(id) },
+        });
+        res.status(200).json(tasks);
+    } catch (errors) {
+        res.status(500).json({ error: 'kan je taak niet vinden' })
+    }
+}
+
+export const createTask = async (req: Request, res: Response) => {
+    const taskName = req.body.name;
+    console.log(taskName);
+    try {
+        const task = await prisma.task.create({
+            data: {
+                name: req.body.name,
+                description: req.body.description,
+                date: req.body.date ? new Date(req.body.date) : undefined,
+                icon: req.body.icon,
+                xp: req.body.xp !== undefined ? Number(req.body.xp) : undefined,
+                coordinates: req.body.coordinates !== undefined ? Number(req.body.coordinates) : undefined,
+                teacherId: req.body.teacherId !== undefined? Number(req.body.teacherId) : undefined
+            }
+        })
+        res.status(200).json(task);
+    } catch (errors) {
+        res.status(500).json({ error: 'kan geen taak aanmaken' });
+    }
+};
+
+export const updateTask = async (req: Request, res: Response) => {
+    try {
+        const id = req.params.id;
+        const task = await prisma.task.update({
+            where:
+                { id: Number(id) },
+            data: {
+                 name: req.body.name,
+                description: req.body.description,
+                date: req.body.date ? new Date(req.body.date) : undefined,
+                icon: req.body.icon,
+                xp: req.body.xp !== undefined ? Number(req.body.xp) : undefined,
+                coordinates: req.body.coordinates !== undefined ? Number(req.body.coordinates) : undefined,
+                teacherId: req.body.teacherId !== undefined? Number(req.body.teacherId) : undefined
+            }
+        })
+       res.status(200).json(task);
+    } catch (errors) {
+        res.status(500).json({ error: 'kan geen taak updaten' });
+    }
+}
+
+export const deleteTask = async (req: Request, res: Response) => {
+     try {
+        const id = req.params.id;
+        const task = await prisma.task.delete({
+            where:
+                { id: Number(id) },
+        })
+       res.status(200).json(task);
+    } catch (errors) {
+        res.status(500).json({ error: 'kan geen taak verwijderen' });
+    }
+}
--- groups/code/src/routes/index.ts
+++ groups/code/src/routes/index.ts
@@ -1,5 +1,6 @@
 import Express, { Router } from 'express';
 import { getTeachers} from '../controllers/groupsController.js';
+import { createTask, deleteTask, getAllTasks, getTaskById, updateTask } from '../controllers/tasksController.ts';
 const router: Router = Express.Router();
 
 // router.get('/', (req: Request, res: Response, next: NextFunction) => {
@@ -7,5 +8,11 @@ const router: Router = Express.Router();
 //   next();
 // });
 router.get('/teachers', getTeachers);
+
+router.get('/tasks/:id', getTaskById);
+router.get('/tasks', getAllTasks);
+router.post('/tasks', createTask);
+router.put('/tasks/:id', updateTask);
+router.delete('/tasks/:id', deleteTask);
 
 export default router;
