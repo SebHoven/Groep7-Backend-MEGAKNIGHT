4ca6ff17c1388cc515cc2258650b8416a3c4edac
Jero <exte0005@hz.nl>
2025-12-09 18:08:56 +0100

    database af

--- groups/code/prisma/types.ts
+++ groups/code/prisma/types.ts
@@ -63,7 +63,7 @@ interface TaskStudent {
 interface Avatar {
   id?: number;
   createdAt?: Date;
-  studentId: number;
+  studentId: number; // reference to Student
   student?: Student;
   avatarHasItem: AvatarHasItem[];
 }
@@ -71,9 +71,9 @@ interface Avatar {
 interface AvatarHasItem {
   id: number;
   createdAt?: Date;
-  avatarId: number;
+  avatarId: number; //reference to Avatar
   avatar: Avatar;
-  avatarItemId: number;
+  avatarItemId: number; // reference to AvatarItem
   avatarItem: AvatarItem;
 }
 
@@ -86,6 +86,37 @@ interface AvatarItem {
   avatarHasItem: AvatarHasItem[];
 }
 
+interface Battlepass {
+  id: number;
+  createdAt: Date;
+  name: string;
+  startDate: Date;
+  endDate: Date;
+  reward?: BattlepassReward[]
+  progress?: BattlepassProgress[]
+}
+
+interface BattlepassReward {
+  id: number;
+  createdAt: Date;
+  level: number;
+  avatarItem: AvatarItem;
+  avatarItemId: number; // reference to avatarItem
+  battlepass: Battlepass;
+  battlepassId: number; // reference to battlepass
+}
+
+interface BattlepassProgress {
+  id: number;
+  createdAt: Date;
+  xp: number;
+  level: number;
+  battlepassId: number; //reference to battlepass
+  studentId: number; //reference to student
+  battlepass: Battlepass;
+  student: Student;
+}
 
 
-export { Teacher, Group, Student, Task, TaskStep, GroupStudent, TaskStudent, Avatar, AvatarHasItem, AvatarItem };
+
+export { Teacher, Group, Student, Task, TaskStep, GroupStudent, TaskStudent, Avatar, AvatarHasItem, AvatarItem, Battlepass, BattlepassReward, BattlepassProgress };
