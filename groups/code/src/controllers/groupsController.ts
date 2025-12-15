95b211bb37ff635480bf0e128a9f9eeb5fd9c567
Korben de Vos <kdevos3@student.scalda.nl>
2025-12-15 16:34:39 +0100

    update voor render 18

--- groups/code/src/controllers/groupsController.ts
+++ groups/code/src/controllers/groupsController.ts
@@ -1,6 +1,6 @@
 import { Request, Response } from 'express';
 import { PrismaClient } from '@prisma/client';
-import { Teacher} from '../prisma/types.ts';
+import { Teacher} from '../prisma/types';
 const prisma: PrismaClient = new PrismaClient();
 
 /**
