92d457ebe37b5bc254d886714737034f3716690c
Korben de Vos <kdevos3@student.scalda.nl>
2026-01-11 22:42:44 +0100

    Seeding, schema, leaderboard en migrations gefixed

--- groups/code/prisma/migrations/20251217095242_groups/migration.sql
+++
@@ -1,8 +0,0 @@
-/*
-  Warnings:
-
-  - A unique constraint covering the columns `[mapId]` on the table `MapState` will be added. If there are existing duplicate values, this will fail.
-
-*/
--- CreateIndex
-CREATE UNIQUE INDEX "MapState_mapId_key" ON "MapState"("mapId");
